ffb2f75325555785d618c9ca020117f86b50d701
Walid Chatelain <walid.chatelain@heig-vd.ch>
2017-02-28 13:28:33 +0100

    user and issue models finished

--- app/models/user.js
+++ app/models/user.js
@@ -13,8 +13,8 @@ const userSchema = new Schema({
   credentials: {
   	username: {type: String, required: true},
   	email: {type: String, required: true},
-  	password: {type: String, required true}
-  	},
+  	password: {type: String, required: true}
+  },
   role: {type: String, required: true},
 });
 
