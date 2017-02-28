c36f07e1b6f07801eae8123b4dc66053469c766b
Walid Chatelain <walid.chatelain@heig-vd.ch>
2017-02-28 11:05:12 +0100

    ajout model user

--- app/models/user.js
+++ app/models/user.js
@@ -1,8 +1,21 @@
+/* User model
+ * A username, an email and a password are used for the authentification
+ * There is also a role for each user : 
+ * - Staff who fix the issues
+ * - Citizen who spot and signal the issues
+*/
+
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 // Define the schema for users
 const userSchema = new Schema({
-  name: String
+  // Define the validation rules for users
+  username: {type: String, required: true},
+  email: {type: String, required: true},
+  password: {type: String, required true},
+  role: {type: String, required: true}
 });
+
+
 // Create the model from the schema and export it
 module.exports = mongoose.model('User', userSchema);
