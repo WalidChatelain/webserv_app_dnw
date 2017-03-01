79c19b3c4648a5b3fc30e1dc06c8b744a33670c6
Walid Chatelain <walid.chatelain@heig-vd.ch>
2017-03-01 11:37:53 +0100

    add apidoc package and files

---
+++ docs/api_project.js
@@ -0,0 +1,14 @@
+define({
+  "name": "my-app",
+  "version": "0.0.0",
+  "description": "",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2017-03-01T09:56:52.045Z",
+    "url": "http://apidocjs.com",
+    "version": "0.17.5"
+  }
+});
